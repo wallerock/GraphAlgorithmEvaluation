4dea2d70e097c53763cc2e9b28578cf1745c5e07
RongLi <rongrong.118@hotmail.com>
2013-11-01 23:52:50 -0700

    graph class implement and test

---
+++ Dijkstra's Algorithm/main.cpp
@@ -0,0 +1,28 @@
+#include"graph.h"
+
+using namespace std;
+
+
+int main(){
+Graph g(10, 0.2, 10);
+
+int vc;
+int ec;
+
+vc=g.V();
+
+ec=g.E();
+
+cout<< vc<<endl;
+
+cout<< ec<<endl;
+
+g.add_edge(3,2);
+
+ec=g.E();
+
+cout<< ec<<endl;
+
+return 0;
+
+}
